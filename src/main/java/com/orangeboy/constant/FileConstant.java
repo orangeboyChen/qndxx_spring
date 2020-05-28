4131b6436c8cafaa45a458e07bb0ab98a8613211
orangeboy <7500638+orangeboy@user.noreply.gitee.com>
2020-05-28 15:21:25 +0800

    Revert "2.7.6debug 添加作业大学习"
    This reverts commit 4820103d

--- src/main/java/com/orangeboy/constant/FileConstant.java
+++ src/main/java/com/orangeboy/constant/FileConstant.java
@@ -2,7 +2,7 @@ package com.orangeboy.constant;
 
 public class FileConstant {
     public final static String PATH = "D:\\QNDXX";
-
+    public final static String TEMP_PATH = "D:\\QNDXX\\temp";
     //ResponseBody
     public final static String NOT_A_SCREENSHOT = "NOT_A_SCREENSHOT";
     public final static String UNEXPECTED_ERROR = "UNEXPECTED_ERROR";
